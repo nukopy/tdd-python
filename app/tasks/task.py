31fcfbaa0a68fffa8803936fceb86700666b5bc5
nukopy <pytwbf201830@gmail.com>
2020-11-16 19:57:29 +0900

    feat: impl "Task" with namedtuple

---
+++ app/tasks/task.py
@@ -0,0 +1,7 @@
+""" Test the "Task" data type. """
+
+from collections import namedtuple
+
+
+Task = namedtuple("Task", ["summary", "owner", "done", "id"])
+Task.__new__.__defaults__ = (None, None, False, None)
